9a8b106d11009e994ecd2799b7726839a734c6b7
NadiaNurHalisa <161995535+NadiaNurHalisa@users.noreply.github.com>
2025-05-12 22:33:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import UserRoute from "./routes/Userroute.js";
+import Userroute from "./routes/Userroute.js";
 import db from "./databases/Databases.js";
 
 const app = express();
